a1412d9391fc2634f089d7ea852782808c6323f3
tinkalguli <tinkalmunu@gmail.com>
2022-05-30 18:52:41 +0530

    Add collapse button and use the new logo in sidebar

--- app/javascript/src/components/Common/Sidebar/index.jsx
+++ app/javascript/src/components/Common/Sidebar/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import { RightArrow, LeftArrow } from "neetoicons";
 import { Sidebar as NeetoUISidebar } from "neetoui/layouts";
 import { useHistory } from "react-router-dom";
 
@@ -13,9 +14,10 @@ import { useAuthDispatch } from "contexts/auth";
 import { useUserState } from "contexts/user";
 
 import { APP_NAME, SIDENAV_LINKS } from "./constants";
+import Logo from "./Logo";
 
 const Sidebar = () => {
-  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
   const history = useHistory();
   const authDispatch = useAuthDispatch();
   const { user } = useUserState();
@@ -46,23 +48,34 @@ const Sidebar = () => {
   ];
 
   return (
-    <NeetoUISidebar
-      isCollapsed={isSidebarCollapsed}
-      navLinks={SIDENAV_LINKS}
-      appName={APP_NAME}
-      organizationInfo={{
-        name: "Wheel",
-        subdomain: "bigbinary.com",
-      }}
-      profileInfo={{
-        name: `${user.first_name} ${user.last_name}`,
-        imageUrl: user.profile_image_path,
-        email: user.email,
-        bottomLinks,
-      }}
-      onCollapse={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
-      changelogProps={{ id: "neetochangelog-trigger" }}
-    />
+    <nav>
+      <NeetoUISidebar
+        isCollapsed={isSidebarCollapsed}
+        navLinks={SIDENAV_LINKS}
+        appName={APP_NAME}
+        organizationInfo={{
+          logo: <Logo />,
+        }}
+        profileInfo={{
+          name: `${user.first_name} ${user.last_name}`,
+          imageUrl: user.profile_image_path,
+          email: user.email,
+          bottomLinks,
+        }}
+        changelogProps={{ id: "neetochangelog-trigger" }}
+      />
+      <div className="collapse-btn flex cursor-pointer justify-center">
+        {isSidebarCollapsed ? (
+          <RightArrow
+            onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+          />
+        ) : (
+          <LeftArrow
+            onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+          />
+        )}
+      </div>
+    </nav>
   );
 };
 
