834b574d44b497cf474028800aa185135d9860ff
tinkalguli <tinkalmunu@gmail.com>
2022-05-31 14:33:04 +0530

    Add VIEWS, SEGMENTS and TAGS values in the constant file of CustomMenuBar

---
+++ app/javascript/src/components/Dashboard/Notes/CustomMenuBar/constants.js
@@ -0,0 +1,18 @@
+export const VIEWS = [
+  { label: "All", count: 200 },
+  { label: "Users", count: 80 },
+  { label: "Leads", count: 60 },
+  { label: "Visitors", count: 60 },
+];
+
+export const SEGMENTS = [
+  { label: "Europe", count: 80 },
+  { label: "Middle-East", count: 60 },
+  { label: "Asia", count: 60 },
+];
+
+export const TAGS = [
+  { label: "Sales", count: 80 },
+  { label: "Finance", count: 60 },
+  { label: "User Experience", count: 60 },
+];
