05beaca945bc089669f0d3a4f4d078b605c5b952
tinkalguli <tinkalmunu@gmail.com>
2022-06-01 19:16:32 +0530

    Add CustomMenuBar and Header in contacts component

--- app/javascript/src/components/Dashboard/Contacts/index.jsx
+++ app/javascript/src/components/Dashboard/Contacts/index.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { Button } from "neetoui";
+import { Container, Header, Scrollable } from "neetoui/layouts";
+
+import CustomMenuBar from "components/Common/CustomMenuBar";
+
+import { CONTACTS_VIEWS } from "./constants";
 import Table from "./Table";
 
-const Contacts = () => <Table />;
+const Contacts = () => {
+  const [, setShowNewContactPane] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div className="flex w-full">
+      <CustomMenuBar
+        title="Contacts"
+        showMenu={showMenu}
+        views={CONTACTS_VIEWS}
+        segments={[]}
+        tags={[]}
+      />
+      <Container>
+        <Header
+          title="All Contacts"
+          actionBlock={
+            <Button
+              onClick={() => setShowNewContactPane(true)}
+              label="Add Contact"
+              icon="ri-add-line"
+            />
+          }
+          searchProps={{
+            value: searchTerm,
+            onChange: e => setSearchTerm(e.target.value),
+            placeholder: "Search Name, Email, Phone, Ect.",
+          }}
+          menuBarToggle={() => setShowMenu(!showMenu)}
+        />
+        <Scrollable className="w-full">
+          <Table />
+        </Scrollable>
+      </Container>
+    </div>
+  );
+};
 
 export default Contacts;
