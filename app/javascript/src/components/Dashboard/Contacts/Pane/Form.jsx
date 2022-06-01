14f979f43368fafaeeb0d008913cf958cd6132f0
tinkalguli <tinkalmunu@gmail.com>
2022-06-01 22:56:31 +0530

    Add ContactForm with email, first_name, last_name and role input field

---
+++ app/javascript/src/components/Dashboard/Contacts/Pane/Form.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+import { Formik, Form } from "formik";
+import { Button, Pane } from "neetoui";
+import { Input, Select } from "neetoui/formik";
+
+import { CONTACTS_FORM_VALIDATION_SCHEMA, ROLES } from "../constants";
+
+export default function ContactForm({ onClose, contact, isEdit }) {
+  const [submitted, setSubmitted] = useState(false);
+
+  return (
+    <Formik
+      initialValues={contact}
+      onSubmit={() => {}}
+      validateOnBlur={submitted}
+      validateOnChange={submitted}
+      validationSchema={CONTACTS_FORM_VALIDATION_SCHEMA}
+    >
+      {({ isSubmitting }) => (
+        <Form className="w-full">
+          <Pane.Body className="space-y-6">
+            <div className="flex w-full flex-grow-0 items-center justify-between">
+              <Input
+                label="First Name"
+                name="first_name"
+                placeholder="Enter first name"
+                className="mr-2"
+                required
+              />
+              <Input
+                label="Last Name"
+                name="last_name"
+                placeholder="Enter last name"
+                className="ml-2"
+                required
+              />
+            </div>
+            <Input
+              label="Email Address"
+              name="email"
+              placeholder="Enter your email address"
+              className="w-full flex-grow-0"
+              required
+            />
+            <Select
+              required
+              label="Role"
+              name="role"
+              className="w-full flex-grow-0"
+              options={ROLES}
+              isSearchable={true}
+              placeholder="Select Role"
+            />
+          </Pane.Body>
+          <Pane.Footer>
+            <Button
+              type="submit"
+              label={isEdit ? "Update" : "Save Changes"}
+              size="large"
+              style="primary"
+              className="mr-3"
+              disabled={isSubmitting}
+              loading={isSubmitting}
+              onClick={() => setSubmitted(true)}
+            />
+            <Button
+              onClick={onClose}
+              label="Cancel"
+              size="large"
+              style="text"
+            />
+          </Pane.Footer>
+        </Form>
+      )}
+    </Formik>
+  );
+}
