7c6778646d380ee0c62a3819b7a9d2eeb5824e7b
tinkalguli <tinkalmunu@gmail.com>
2022-06-01 19:13:54 +0530

    Add CONTACTS_VIEWS in the constant file of Contacts component

--- app/javascript/src/components/Dashboard/Contacts/constants.js
+++ app/javascript/src/components/Dashboard/Contacts/constants.js
@@ -7,28 +7,103 @@ import { formatDate } from "utils/time";
 
 export const CONTACTS = [
   {
-    name: "Ronald Richards",
-    email: "albert@borer.com",
+    email: "Barry_Taylor3408@twace.org",
+    name: "Barry Taylor",
     created_at: "2022-05-31T12:38:32.806Z",
   },
   {
-    name: "Jacob Jones",
-    email: "jacob@borer.com",
+    email: "Chuck_Dyson5768@atink.com",
+    name: "Chuck Dyson",
     created_at: "2022-05-31T12:38:32.806Z",
   },
   {
-    name: "John Smith",
-    email: "john@borer.com",
+    email: "Eduardo_Gunn5778@gembat.biz",
+    name: "Eduardo_Gunn5778",
     created_at: "2022-05-31T12:38:32.806Z",
   },
   {
-    name: "Jack Snow",
-    email: "jack@borer.com",
+    email: "Eduardo_Allcott9101@sveldo.biz",
+    name: "Eduardo Allcott",
     created_at: "2022-05-31T12:38:32.806Z",
   },
   {
-    name: "Nalson Richards",
-    email: "nalson@borer.com",
+    email: "Nathan_Khan8258@bungar.biz",
+    name: "Nathan Khan",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Zoe_Terry2465@nanoff.biz",
+    name: "Zoe Terry",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Cecilia_Watt8135@naiker.biz",
+    name: "Cecilia Watt",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Jennifer_Lewis9652@gmail.com",
+    name: "Jennifer Lewis",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Lana_Shields2803@cispeto.com",
+    name: "Lana Shields",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Julian_Cunningham2816@zorer.org",
+    name: "Julian Cunningham",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Diane_Hilton5577@mafthy.com",
+    name: "Diane Hilton",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Mason_Goodman4082@eirey.tech",
+    name: "Mason Goodman",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Candace_Lindsay3978@atink.com",
+    name: "Candace Lindsay",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Harry_Miller7846@extex.org",
+    name: "Harry Miller",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Alexander_Palmer5366@bungar.biz",
+    name: "Alexander Palmer",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Johnathan_Porter3237@iatim.tech",
+    name: "Johnathan Porter",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Alexander_Ring3679@eirey.tech",
+    name: "Alexander Ring",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Analise_Neville2234@deavo.com",
+    name: "Analise Neville",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Carter_Walter7155@nanoff.biz",
+    name: "Carter Walter",
+    created_at: "2022-05-31T12:38:32.806Z",
+  },
+  {
+    email: "Eryn_Nanton1351@muall.tech",
+    name: "Eryn Nanton",
     created_at: "2022-05-31T12:38:32.806Z",
   },
 ];
@@ -71,4 +146,11 @@ export const CONTACTS_TABLE_COLUMN_DATA = [
     key: "icon",
     render: () => <MenuHorizontal />,
   },
+];
+
+export const CONTACTS_VIEWS = [
+  { label: "All", count: 200 },
+  { label: "Archived", count: 80 },
+  { label: "Completed", count: 60 },
+  { label: "Phase 2", count: 60 },
 ];
